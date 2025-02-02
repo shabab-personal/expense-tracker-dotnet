73ba3604a23ede8e5f5e5f09e57b3cfc9b91ddea
ssal6113 <ssal6113@uni.sydney.edu.au>
2025-02-02 15:01:48 +0600

    first commit, created few classes in models

---
+++ expense-tracker-dotnet/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace expense_tracker_dotnet.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        { 
+
+        }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ expense-tracker-dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace expense_tracker_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
